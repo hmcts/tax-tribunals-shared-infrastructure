ab1535eeb063be78fcefdd0838c6676f1d72c114
Will Taylor <wrftaylor@gmail.com>
2023-05-11 11:36:26 +0100

    add new inputs

--- variables-inputs.tf
+++ variables-inputs.tf
@@ -10,6 +10,8 @@ variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault."
 }
 
+# DB and Keyvault
+
 variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
