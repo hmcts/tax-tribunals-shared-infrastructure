e69bed87c29d2761dcc91d5212336050050df57c
Will Taylor <wrftaylor@gmail.com>
2023-05-15 11:44:57 +0100

    delete everything

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ terraform {
   backend "azurerm" {}
 }
 
-provider "azurerm" {
-  features {}
-}
+# provider "azurerm" {
+#   features {}
+# }
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "postgres_network"
-  subscription_id            = var.aks_subscription_id
-}
+# provider "azurerm" {
+#   features {}
+#   skip_provider_registration = true
+#   alias                      = "postgres_network"
+#   subscription_id            = var.aks_subscription_id
+# }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
-  location = var.location
-  tags     = var.common_tags
-}
+# resource "azurerm_resource_group" "rg" {
+#   name     = "${var.product}-${var.env}"
+#   location = var.location
+#   tags     = var.common_tags
+# }
