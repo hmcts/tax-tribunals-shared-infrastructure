7e9cce6590305220fee548e21c20ec285bf8b8ee
Will Taylor <wrftaylor@gmail.com>
2023-05-15 11:36:02 +0100

    update db values

--- db.tf
+++ db.tf
@@ -25,32 +25,32 @@ module "tt-database" {
 
 # # Add DB outputs to keyvault
 
-# resource "azurerm_key_vault_secret" "tt-postgres-user" {
-#   name         = "tt-postgres-user"
-#   value        = module.tt-database.username
-#   key_vault_id = module.tt-key-vault.key_vault_id
-# }
+resource "azurerm_key_vault_secret" "tt-postgres-user" {
+  name         = "tt-postgres-user"
+  value        = module.tt-database.username
+  key_vault_id = module.tt-key-vault.key_vault_id
+}
 
-# resource "azurerm_key_vault_secret" "tt-postgres-password" {
-#   name         = "tt-postgres-password"
-#   value        = module.tt-database.password
-#   key_vault_id = module.tt-key-vault.key_vault_id
-# }
+resource "azurerm_key_vault_secret" "tt-postgres-password" {
+  name         = "tt-postgres-password"
+  value        = module.tt-database.password
+  key_vault_id = module.tt-key-vault.key_vault_id
+}
 
-# # resource "azurerm_key_vault_secret" "tt-postgres-host" {
-# #   name         = "tt-postgres-host"
-# #   value        = module.tt-database.host_name
-# #   key_vault_id = module.tt-key-vault.key_vault_id
-# # }
+resource "azurerm_key_vault_secret" "tt-postgres-host" {
+  name         = "tt-postgres-host"
+  value        = module.tt-database.host_name
+  key_vault_id = module.tt-key-vault.key_vault_id
+}
 
-# # resource "azurerm_key_vault_secret" "tt-postgres-port" {
-# #   name         = "tt-postgres-port"
-# #   value        = module.tt-database.postgresql_listen_port
-# #   key_vault_id = module.tt-key-vault.key_vault_id
-# # }
+resource "azurerm_key_vault_secret" "tt-postgres-port" {
+  name         = "tt-postgres-port"
+  value        = module.tt-database.postgresql_listen_port
+  key_vault_id = module.tt-key-vault.key_vault_id
+}
 
-# # resource "azurerm_key_vault_secret" "tt-postgres-database" {
-# #   name         = "tt-postgres-database"
-# #   value        = module.tt-database.postgresql_database
-# #   key_vault_id = module.tt-key-vault.key_vault_id
-# # }
+resource "azurerm_key_vault_secret" "tt-postgres-database" {
+  name         = "tt-postgres-database"
+  value        = module.tt-database.postgresql_database
+  key_vault_id = module.tt-key-vault.key_vault_id
+}
