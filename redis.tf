6ef5837170ccf37ebc8f60d579ca1079a0530146
Will Taylor <wrftaylor@gmail.com>
2023-05-18 10:32:12 +0100

    update redis subnet

--- redis.tf
+++ redis.tf
@@ -3,12 +3,12 @@ module "redis" {
   product     = var.product
   location    = var.location
   env         = var.env
-  subnetid    = data.azurerm_subnet.redis.id
+  subnetid    = data.azurerm_subnet.core_infra_redis_subnet.id
   common_tags = var.common_tags
 }
 
-data "azurerm_subnet" "redis" {
-  name                 = "core-infra-subnet-tax-tribunals-${var.env}"
+data "azurerm_subnet" "core_infra_redis_subnet" {
+  name                 = "core-infra-subnet-1-${var.env}"
   virtual_network_name = "core-infra-vnet-${var.env}"
   resource_group_name = "core-infra-${var.env}"
 }
