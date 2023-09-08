848ef0003e0f90268a8803f137db382317422832
Will Taylor <wrftaylor@gmail.com>
2023-09-08 11:21:36 +0100

    update keyvault name

--- key-vault.tf
+++ key-vault.tf
@@ -1,6 +1,6 @@
 module "tt-key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name                    = "${var.product}-${var.env}"
+  name                    = "${var.product}-cft-${var.env}"
   product                 = var.product
   env                     = var.env
   tenant_id               = var.tenant_id
